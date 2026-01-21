736ef0c0755516b260685cd23abe0d8e794bc7c1
RyanTE25 <shaumypr@gmail.com>
2026-01-21 11:35:23 +0700

    auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,22 +1,37 @@
-import { createClient } from
-"https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { supabase } from "./supabase.js";
 
-const supabase = createClient(
-  "https://pjlxqdlbvtzgijjnwcql.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqbHhxZGxidnR6Z2lqam53Y3FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0NDMzNzgsImV4cCI6MjA4NDAxOTM3OH0.6htasW1ob6fxFPNQtZjr7It9ztbOkjNE0sDpAaSBmuw"
-);
+export async function cekLogin() {
+  const { data: { session } } = await supabase.auth.getSession();
+  if (!session) location.href = "login.html";
+  return session;
+}
 
-async function login() {
+export async function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const { error } = await supabase.auth.signInWithPassword({
-    email,
-    password
-  });
+  const { error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) return alert(error.message);
 
-  if (error) alert(error.message);
-  else window.location.href = "index.html";
+  location.href = "index.html";
+}
+
+export async function getRole() {
+  const session = await cekLogin();
+
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("role")
+    .eq("id", session.user.id)
+    .single();
+
+  if (error) throw error;
+  return data.role;
+}
+
+export async function logout() {
+  await supabase.auth.signOut();
+  location.href = "login.html";
 }
 
 window.login = login;
